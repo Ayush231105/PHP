3daa0388f89ba0a29fd034414b8a7936090e7589
Ayush231105 <mukherjeeayush664@gmail.com>
2026-08-31 15:44:16 +0530

    Add files via upload

---
+++ q2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Name Form</title>
+</head>
+<body>
+    <form action="q2.php" method="post">
+        Enter Your Name:
+        <input type="text" name="username">
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
+<?php
+$name = $_POST["username"];
+echo "Your Name is: " . $name;
+?>
